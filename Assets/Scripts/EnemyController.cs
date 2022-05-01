4ddbc5c89daf12e640ba9da36485dda65b4baf98
fihra <fabrofabian@gmail.com>
2022-04-30 21:35:46 -1000

    enemies can shoot

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -6,14 +6,23 @@ public class EnemyController : MonoBehaviour
 {
     Rigidbody2D rb;
     public Transform target;
+    public GameObject projectile;
+    public GameObject shootingNozzle;
 
     [Range(1.0f, 100.0f)]
-    public float speed = 50.0f;
+    public float moveSpeed = 25.0f;
+
+    [Range(0.01f, 10.0f)]
+    public float secondsToAttack = 1f;
+
+    [Range(0.05f, 10.0f)]
+    public float shotSpeed = 1.0f;
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        InvokeRepeating("Shooting", 2.5f, secondsToAttack);
     }
 
     // Update is called once per frame
@@ -22,11 +31,18 @@ public class EnemyController : MonoBehaviour
         Moving();
     }
 
+    void Shooting()
+    {
+        Vector2 projectilePosition = shootingNozzle.transform.position;
+        GameObject shot = Instantiate(projectile, projectilePosition, Quaternion.identity);
+        shot.transform.position = projectilePosition + Vector2.down * shotSpeed * Time.deltaTime;
+    }
+
     void Moving()
     {
         //transform.Translate(Vector2.down * speed * Time.deltaTime);
         //transform.position = Vector2.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
-        rb.AddForce(Vector2.down * speed * Time.deltaTime);
+        rb.AddForce(Vector2.down * moveSpeed * Time.deltaTime);
         if (rb.position.y <= -6.0f)
         {
             Destroy(gameObject);
